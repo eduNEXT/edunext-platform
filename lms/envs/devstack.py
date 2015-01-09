b740629843af3f81e25b654419e4f3377811b3e9
Laq <leonardo.quinonez@edunext.co>
2015-01-08 19:26:56 -0500

    setting default language for the application

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -16,6 +16,10 @@ SITE_NAME = 'localhost:8000'
 # By default don't use a worker, execute tasks as if they were local functions
 CELERY_ALWAYS_EAGER = True
 
+###############################LANG########################
+LANGUAGE_CODE = 'es-419'
+TIME_ZONE = 'America/Bogota'
+
 ################################ LOGGERS ######################################
 
 import logging
