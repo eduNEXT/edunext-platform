3a9e860ebc7289bf28610834ab4e66809280a8e1
Jhony Avella <jhony.avella@edunext.co>
2016-02-03 14:49:11 -0500

    Adding necessary template tags
    From cc9e441 dg

--- common/djangoapps/microsite_configuration/templatetags/microsite.py
+++ common/djangoapps/microsite_configuration/templatetags/microsite.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from microsite_configuration import microsite
 from django.templatetags.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils.translation import get_language_bidi
 
 register = template.Library()
 
@@ -59,7 +60,31 @@ def microsite_css_overrides_file():
     {% microsite_css_overrides_file %}
     """
     file_path = microsite.get_value('css_overrides_file', None)
+    if get_language_bidi():
+        file_path = microsite.get_value(
+            'css_overrides_file_rtl',
+            microsite.get_value('css_overrides_file')
+        )
+    else:
+        file_path = microsite.get_value('css_overrides_file')
+
     if file_path is not None:
         return "<link href='{}' rel='stylesheet' type='text/css'>".format(static(file_path))
     else:
         return ""
+
+
+@register.simple_tag(name="microsite_rtl")
+def microsite_rtl_tag():
+    """
+    Django template tag that outputs the direction string for rtl support
+    """
+    return 'rtl' if get_language_bidi() else 'ltr'
+
+
+@register.filter
+def microsite_template_path(template_name):
+    """
+    Django template filter to apply template overriding to microsites
+    """
+    return microsite.get_template_path(template_name)
