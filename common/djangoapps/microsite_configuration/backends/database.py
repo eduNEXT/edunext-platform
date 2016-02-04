505d0c042c5087d66894d9e5a2af497154203d38
Jhony Avella <jhony.avella@edunext.co>
2016-02-04 17:45:22 -0500

    Enabling specific microsite translations for both filebased and database backends
    adding translations to microsites which could use context for specific translations
    
    From cf2e0b6 dg
    
    add microsite translations to file based backend
    
    From de762f6 dg

--- common/djangoapps/microsite_configuration/backends/database.py
+++ common/djangoapps/microsite_configuration/backends/database.py
@@ -106,6 +106,7 @@ class DatabaseMicrositeBackend(SettingsFileMicrositeBackend):
             settings.DEFAULT_TEMPLATE_ENGINE['DIRS'].append(microsites_root)
             edxmako.paths.add_lookup('main', microsites_root)
             settings.STATICFILES_DIRS.insert(0, microsites_root)
+            settings.LOCALE_PATHS = (microsites_root / 'conf/locale',) + settings.LOCALE_PATHS
 
             log.info('Loading microsite path at %s', microsites_root)
         else:
