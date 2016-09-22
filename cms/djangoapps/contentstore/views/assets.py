7b11e5bf512a41d3d9ee7a282d050ee56e852669
Pedro Rodriguez <pedro.rodriguez@edunext.co>
2016-09-22 16:20:42 -0500

    Making the link to the assets microsite aware
    (cherry picked from commit 1322d216b9403c17968549ce7088a63b5b1571f8)

--- cms/djangoapps/contentstore/views/assets.py
+++ cms/djangoapps/contentstore/views/assets.py
@@ -29,6 +29,9 @@ from pymongo import ASCENDING, DESCENDING
 from student.auth import has_course_author_access
 from xmodule.modulestore.exceptions import ItemNotFoundError
 
+# eduNEXT 14.07.2016
+from microsite_configuration import microsite
+
 __all__ = ['assets_handler']
 
 # pylint: disable=unused-argument
@@ -370,7 +373,9 @@ def _get_asset_json(display_name, content_type, date, location, thumbnail_locati
     Helper method for formatting the asset information to send to client.
     """
     asset_url = StaticContent.serialize_asset_key_with_slash(location)
-    external_url = settings.LMS_BASE + asset_url
+    # eduNEXT 14.07.2016
+    lms_base = microsite.get_value_for_org(location.org, 'SITE_NAME', settings.LMS_BASE)
+    external_url = lms_base + asset_url
     return {
         'display_name': display_name,
         'content_type': content_type,
