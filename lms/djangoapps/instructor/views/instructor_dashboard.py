c8371f3d0cb11dca84309fe009b762e05bf2ecd3
FelipeMontoya <felipe.montoya@edunext.co>
2017-03-06 11:57:24 +0100

    FEC-12: Allowing instructors and not just staff to manage certificates on the instructor tab ---
    Add to instructor the option for invalidate/revalidate certificates
    CERTIFICATES_INSTRUCTOR_GENERATION has to be activated on each microsite from 3e0a0822727069ed04afd54f32366339a86df484
    Changing the permission, to something that can be activated in studio
    Lowering the barrier for certificate generation to instructor level

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -18,7 +18,7 @@ from edxmako.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.utils.html import escape
 from django.http import Http404, HttpResponseServerError
-from django.conf import settings
+from openedx.conf import settings
 from util.json_request import JsonResponse
 from mock import patch
 
@@ -187,7 +187,8 @@ def instructor_dashboard_2(request, course_id):
     # and enable self-generated certificates for a course.
     # Note: This is hidden for all CCXs
     certs_enabled = CertificateGenerationConfiguration.current().enabled and not hasattr(course_key, 'ccx')
-    if certs_enabled and access['admin']:
+    # eduNEXT changed on 14.07.2016 to allow access to the course-administrators (as per studio)
+    if certs_enabled and access['instructor']:
         sections.append(_section_certificates(course))
 
     disable_buttons = not _is_small_course(course_key)
