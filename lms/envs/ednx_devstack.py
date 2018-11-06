7368fd9439aaa7013cecdc8803aef37bbb66b4a3
Luis Moreno <luis.moreno@edunext.co>
2018-11-06 13:32:57 -0400

    trackinglogs in docker devstack

--- lms/envs/ednx_devstack.py
+++ lms/envs/ednx_devstack.py
@@ -1,3 +1,13 @@
 """ Overrides for Docker-based eduNEXT devstack. """
 
 from .devstack_docker import *  # pylint: disable=wildcard-import, unused-wildcard-import
+
+
+LOGGING['handlers']['tracking'] = {
+    'level': 'DEBUG',
+    'class': 'logging.FileHandler',
+    'filename': '/edx/var/log/tracking/tracking.log',
+    'formatter': 'raw',
+    }
+
+LOGGING['loggers']['tracking']['handlers'] = ['tracking']
