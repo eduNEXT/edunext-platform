08d0e4a5b8d77d1ac76a889bd080cb4810a8ba3b
Felipe Montoya <felipe.montoya@edunext.co>
2018-12-20 18:18:57 -0500

    Settings that make lms_worker work

--- lms/envs/ednx_devstack.py
+++ lms/envs/ednx_devstack.py
@@ -23,3 +23,32 @@ TIME_ZONE = 'America/Bogota'
 FEATURES['SHOW_HEADER_LANGUAGE_SELECTOR'] = True
 FEATURES['SHOW_FOOTER_LANGUAGE_SELECTOR'] = True
 FEATURES['EDNX_SITE_AWARE_LOCALE'] = True
+
+
+################################ ASYNC WORKERS ################################
+
+# Require a separate celery worker
+CELERY_ALWAYS_EAGER = False
+
+# Disable transaction management because we are using a worker. Views
+# that request a task and wait for the result will deadlock otherwise.
+for database_name in DATABASES:
+    DATABASES[database_name]['ATOMIC_REQUESTS'] = False
+
+
+CELERY_BROKER_HOSTNAME = "edx.devstack.rabbit"
+CELERY_BROKER_VHOST = "devstack"
+CELERY_BROKER_USER = "celery"
+CELERY_BROKER_PASSWORD = "celery"
+
+BROKER_URL = "{0}://{1}:{2}@{3}/{4}".format(CELERY_BROKER_TRANSPORT,
+                                            CELERY_BROKER_USER,
+                                            CELERY_BROKER_PASSWORD,
+                                            CELERY_BROKER_HOSTNAME,
+                                            CELERY_BROKER_VHOST)
+
+GRADES_DOWNLOAD = {
+    'STORAGE_TYPE': 'localfs',
+    'BUCKET': 'openedx-grades',
+    'ROOT_PATH': '/edx/var/edxapp/uploads',
+}
