a4094ba8d377ed693f8924f4725652ac5a504b39
Andrey Canon <andrey.canon@edunext.co>
2019-12-09 16:02:22 -0500

    remove TemplateLookupTests

--- lms/tests.py
+++ lms/tests.py
@@ -9,8 +9,6 @@ from django.urls import reverse
 from mock import patch
 from six import text_type
 
-from edxmako import LOOKUP, add_lookup
-from microsite_configuration import microsite
 from openedx.features.course_experience import course_home_url_name
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
@@ -37,25 +35,6 @@ class LmsModuleTests(TestCase):
         assert settings.FEATURES['ENABLE_API_DOCS']
         response = self.client.get('/api-docs/')
         self.assertEqual(200, response.status_code)
-
-
-class TemplateLookupTests(TestCase):
-    """
-    Tests for TemplateLookup.
-    """
-    shard = 5
-
-    def test_add_lookup_to_main(self):
-        """Test that any template directories added are not cleared when microsites are enabled."""
-
-        add_lookup('main', 'external_module', __name__)
-        directories = LOOKUP['main'].directories
-        self.assertEqual(len([directory for directory in directories if 'external_module' in directory]), 1)
-
-        # This should not clear the directories list
-        microsite.enable_microsites(log)
-        directories = LOOKUP['main'].directories
-        self.assertEqual(len([directory for directory in directories if 'external_module' in directory]), 1)
 
 
 @patch.dict('django.conf.settings.FEATURES', {'ENABLE_FEEDBACK_SUBMISSION': True})
