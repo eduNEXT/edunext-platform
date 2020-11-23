797a43a97b6d223ef208c03fc10f4e3863e3ba05
Alejandro Cardenas <alecar.main@gmail.com>
2020-11-23 08:35:35 -0500

    fix: changed error messages
    Username and email error messages updated to match customer's requirements.

--- openedx/core/djangoapps/user_authn/views/registration_form.py
+++ openedx/core/djangoapps/user_authn/views/registration_form.py
@@ -141,7 +141,9 @@ class UsernameField(forms.CharField):
             allowed_patterns = settings.REGISTRATION_USERNAME_PATTERNS_ALLOWED
             
             if not any(re.match(pattern + "$", value) for pattern in allowed_patterns):
-                raise ValidationError(_(u"Unauthorized username."))
+                raise ValidationError(_(
+                    u"Unauthorized username, If you are a Técnico user (student, professor or staff) please register via Técnico-ID"
+                ))
 
         return super().clean(value)
 
@@ -271,7 +273,9 @@ class AccountCreationForm(forms.Form):
                 # they may have been manually invited by an instructor and if not,
                 # reject the registration.
                 if not CourseEnrollmentAllowed.objects.filter(email=email).exists():
-                    raise ValidationError(_("Unauthorized email address."))
+                    raise ValidationError(_(
+                        u"Unauthorized email, If you are a Técnico user (student, professor or staff) please register via Técnico-ID"
+                    ))
         if email_exists_or_retired(email):
             raise ValidationError(
                 _(
