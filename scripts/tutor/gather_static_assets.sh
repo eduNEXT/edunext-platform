946c2bd41377e75d348c29e2e3934282774340e4
Eric Herrera <eric.herrera@edunext.co>
2021-03-26 17:47:10 -0500

    Fix gather static assets script.

--- scripts/tutor/gather_static_assets.sh
+++ scripts/tutor/gather_static_assets.sh
@@ -5,15 +5,15 @@ set -x
 THEME_DIRS=${THEME_DIRS:-/openedx/themes/ednx-saas-themes/edx-platform}
 THEMES=${THEMES:-bragi}
 
-chmod a+x /openedx/bin/*
-export PATH=/openedx/bin:${PATH}
 export NO_PYTHON_UNINSTALL=1
 export NO_PREREQ_INSTALL=1
 
-openedx-assets xmodule \
-    && openedx-assets npm \
-    && openedx-assets webpack --env=prod \
-    && openedx-assets common
+cd /openedx/edx-platform
+
+# openedx-assets xmodule \
+#     && openedx-assets npm \
+#     && openedx-assets webpack --env=prod \
+#     && openedx-assets common
 
 openedx-assets themes --theme-dirs $THEME_DIRS --themes $THEMES  \
     && openedx-assets collect --settings=tutor.assets
