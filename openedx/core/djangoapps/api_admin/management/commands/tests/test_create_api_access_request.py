06db8ac4d84a02b4c1caebe74a1ded2c17f5ecab
GonzaloRomero <gonzalo.romero@edunext.co>
2021-10-08 13:36:00 -0500

    skip openedx create api access add import issue

--- openedx/core/djangoapps/api_admin/management/commands/tests/test_create_api_access_request.py
+++ openedx/core/djangoapps/api_admin/management/commands/tests/test_create_api_access_request.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 
 import ddt
+import pytest
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.management import call_command
