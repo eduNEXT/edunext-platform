b9a7b7aa9cd8f298a75d1eae703415fd88c5cf42
Maria Grimaldi <maria.grimaldi@edunext.co>
2021-11-26 16:47:56 -0400

    feat: add first batch of Open edX Filters
    * Add PreEnrollmentFilter
    * Add PreRegisterFilter
    * Add PreLoginFilter
    
    (cherry picked from commit f29a4eef68cc8d8ebbce21025b37db6566997225)

--- common/djangoapps/student/tests/test_filters.py
+++ common/djangoapps/student/tests/test_filters.py
@@ -2,16 +2,14 @@
 Test that various filters are fired for models in the student app.
 """
 from django.test import override_settings
-from openedx_filters.learning.enrollment import PreEnrollmentFilter
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+from openedx_filters.learning.filters import CourseEnrollmentStarted
 from openedx_filters import PipelineStep
 
 from common.djangoapps.student.models import CourseEnrollment, EnrollmentNotAllowed
 from common.djangoapps.student.tests.factories import UserFactory, UserProfileFactory
-
 from openedx.core.djangolib.testing.utils import skip_unless_lms
-
-from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
-from xmodule.modulestore.tests.factories import CourseFactory
 
 
 class TestEnrollmentPipelineStep(PipelineStep):
@@ -19,10 +17,10 @@ class TestEnrollmentPipelineStep(PipelineStep):
     Utility function used when getting steps for pipeline.
     """
 
-    def run(self, user, course_key, mode):  # pylint: disable=unused-argument, arguments-differ
+    def run_filter(self, user, course_key, mode):  # pylint: disable=arguments-differ
         """Pipeline steps that changes mode to honor."""
         if mode == "no-id-professional":
-            raise PreEnrollmentFilter.PreventEnrollment()
+            raise CourseEnrollmentStarted.PreventEnrollment()
         return {"mode": "honor"}
 
 
@@ -33,7 +31,7 @@ class EnrollmentFiltersTest(ModuleStoreTestCase):
 
     This class guarantees that the following filters are triggered during the user's enrollment:
 
-    - PreEnrollmentFilter
+    - CourseEnrollmentStarted
     """
 
     def setUp(self):  # pylint: disable=arguments-differ
@@ -62,7 +60,7 @@ class EnrollmentFiltersTest(ModuleStoreTestCase):
         enrollment process.
 
         Expected result:
-            - PreEnrollmentFilter is triggered and executes TestEnrollmentPipelineStep.
+            - CourseEnrollmentStarted is triggered and executes TestEnrollmentPipelineStep.
             - The arguments that the receiver gets are the arguments used by the filter
             with the enrollment mode changed.
         """
@@ -85,8 +83,22 @@ class EnrollmentFiltersTest(ModuleStoreTestCase):
         Test prevent the user's enrollment through a pipeline step.
 
         Expected result:
-            - PreEnrollmentFilter is triggered and executes TestEnrollmentPipelineStep.
+            - CourseEnrollmentStarted is triggered and executes TestEnrollmentPipelineStep.
             - The user can't enroll.
         """
         with self.assertRaises(EnrollmentNotAllowed):
             CourseEnrollment.enroll(self.user, self.course.id, mode='no-id-professional')
+
+    @override_settings(OPEN_EDX_FILTERS_CONFIG={})
+    def test_enrollment_without_filter_configuration(self):
+        """
+        Test usual enrollment process, without filter's intervention.
+
+        Expected result:
+            - CourseEnrollmentStarted does not have any effect on the enrollment process.
+            - The enrollment process ends successfully.
+        """
+        enrollment = CourseEnrollment.enroll(self.user, self.course.id, mode='audit')
+
+        self.assertEqual('audit', enrollment.mode)
+        self.assertTrue(CourseEnrollment.is_enrolled(self.user, self.course.id))
