d94f6d78766ccc21b2b7ca27c79f2385196049a4
Ivo Branco <ivo.branco@fccn.pt>
2022-03-24 11:37:04 +0000

    Improve extension point error logging

--- openedx/core/djangoapps/plugins/plugin_extension_points.py
+++ openedx/core/djangoapps/plugins/plugin_extension_points.py
@@ -32,12 +32,12 @@ def run_extension_point(extension_point, *args, **kwargs):
         module = import_module(module)
         extension_function = getattr(module, func_name)
         return extension_function(*args, **kwargs)
-    except ImportError:
-        log.info('Could not import the %s : %s', extension_point, module)
+    except ImportError as e:
+        log.error('Could not import the extension point %s : %s with message: %s', extension_point, module, str(e))
         return
     except AttributeError:
-        log.info('Could not import the function %s in the module %s', func_name, module)
+        log.error('Could not import the function %s in the module %s', func_name, module)
         return
     except ValueError:
-        log.info('Could not load the information from \"%s\"', path)
+        log.error('Could not load the information from \"%s\"', path)
         return
