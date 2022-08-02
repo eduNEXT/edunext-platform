d423887d88552aad7657dea67ac57038eecc6d70
johanv26 <johan.castiblanco@edunext.co>
2022-08-02 22:46:49 +0200

    fix: change `LANGUAGE_CODE`conflict for tenant
    This helps to avoid the conflict of the var language_code. This to help the language selector to be change
    and also the default language_code works. If the cookie of language preference works you dont need the `LANGUAGE_SESSION_KEY`
    
    (cherry picked from commit afa3b12135f268cf1eb2f8dad33ce023970be839)

--- openedx/core/djangoapps/dark_lang/middleware.py
+++ openedx/core/djangoapps/dark_lang/middleware.py
@@ -108,7 +108,7 @@ class DarkLangMiddleware(MiddlewareMixin):
         Apply language specified in site configuration.
         """
         language = get_value('LANGUAGE_CODE', None)
-        if language:
+        if language and not request.COOKIES.get(settings.LANGUAGE_COOKIE, None):
             request.session[LANGUAGE_SESSION_KEY] = language
             set_language_cookie(request, response, language)
 
