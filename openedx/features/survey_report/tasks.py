b0bbde3d6f87713f255fc98393b9e18cdce17776
Alejandro Cardenas <alecar.main@gmail.com>
2023-06-24 12:56:20 -0700

    feat: add cache lock to survey report task

--- openedx/features/survey_report/tasks.py
+++ openedx/features/survey_report/tasks.py
@@ -6,12 +6,14 @@ Tasks for Survey Report.
 import logging
 
 from celery import shared_task
+from cache_lock import cache_lock
 from .api import generate_report
 
 log = logging.getLogger('edx.celery.task')
 
 
 @shared_task(name='openedx.features.survey_report.tasks.generate_survey_report')
+@cache_lock("generate_survey_report_lock", expire=60)
 def generate_survey_report():
     """
     Tasks to generate a new survey report with non-sensitive data.
