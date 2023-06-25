677e647ef16307f8e809d8a4d4720a1308f4f107
Alejandro Cardenas <alecar.main@gmail.com>
2023-06-25 16:59:34 -0700

    feat: add acquired_lock

--- openedx/features/survey_report/tasks.py
+++ openedx/features/survey_report/tasks.py
@@ -6,24 +6,29 @@ Tasks for Survey Report.
 import logging
 
 from celery import shared_task
-from django_lock import lock
+from django.core.cache import cache
 from .api import generate_report
 
 log = logging.getLogger('edx.celery.task')
 
 
 @shared_task(name='openedx.features.survey_report.tasks.generate_survey_report')
-@lock("generate_survey_report_lock")
 def generate_survey_report():
     """
     Tasks to generate a new survey report with non-sensitive data.
     """
-    log.info(
-        'Started - generate survey report'
-    )
+    lock_key = 'generate_survey_report_lock'
+    acquired_lock = cache.add(lock_key, 'locked', 60)
 
-    try:
-        generate_report()
-        log.info('Done - generate survey report')
-    except (Exception, ):  # pylint: disable=broad-except
-        log.error('Error - generate survey report')
+    if acquired_lock:
+        log.info(
+            'Started - generate survey report'
+        )
+        try:
+            generate_report()
+            log.info('Done - generate survey report')
+        except (Exception, ):  # pylint: disable=broad-except
+            log.error('Error - generate survey report')
+        cache.delete(lock_key)
+    else:
+        log.warning('Warning - other worker is running this job')
