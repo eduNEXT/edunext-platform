13e19c0c80e1e2ab401d641541f315c2cc534177
Gi-ron <steven.giron@utp.edu.co>
2024-04-08 16:46:26 -0500

    feat: delete contact button for not activate user

--- common/djangoapps/student/views/dashboard.py
+++ common/djangoapps/student/views/dashboard.py
@@ -580,7 +580,6 @@ def student_dashboard(request):  # lint-amnesty, pylint: disable=too-many-statem
     if not user.is_active:
         activate_account_message = Text(_(
             "Check your {email_start}{email}{email_end} inbox for an account activation link from {platform_name}. "
-            "If you need help, contact {link_start}{platform_name} Support{link_end}."
         )).format(
             platform_name=platform_name,
             email_start=HTML("<strong>"),
